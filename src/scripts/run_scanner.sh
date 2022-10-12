8e457d60a4ba94fdc8cdd4ab45f4cf54fe39199e
Martin PAUCOT <m.paucot@kmp.agency>
2022-10-12 13:35:37 +0200

    fix: change command name to run scanner

--- src/scripts/run_scanner.sh
+++ src/scripts/run_scanner.sh
@@ -3,4 +3,4 @@
 cd "$PARAM_DIRECTORY" || exit
 
 export SONAR_SCANNER_OPTS="$PARAM_OPTS"
-eval "scanner -Dsonar.login=\$${PARAM_LOGIN} ${PARAM_EXTRA_ARGS}"
+eval "sonar-scanner -Dsonar.login=\$${PARAM_LOGIN} ${PARAM_EXTRA_ARGS}"
