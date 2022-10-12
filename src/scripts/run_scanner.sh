46460606a1ef4d73512898321b6e95d98c8c039f
Martin PAUCOT <m.paucot@kmp.agency>
2022-10-12 13:31:35 +0200

    fix: fix sonar scanner arguments

--- src/scripts/run_scanner.sh
+++ src/scripts/run_scanner.sh
@@ -3,4 +3,4 @@
 cd "$PARAM_DIRECTORY" || exit
 
 export SONAR_SCANNER_OPTS="$PARAM_OPTS"
-eval scanner "-Dsonar.login=${!$PARAM_LOGIN}"
+eval "scanner -Dsonar.login=\$${PARAM_LOGIN} ${PARAM_EXTRA_ARGS}"
